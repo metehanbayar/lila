68487ff24f1c05e6445808aca200a53391a3fdb8
Metehan Bayar <metehan.bayar@gmail.com>
2025-03-04 17:11:50 +0300

    plesk için

--- server.js
+++ server.js
@@ -1,12 +1,10 @@
 const { createServer } = require('http');
 const { parse } = require('url');
 const next = require('next');
-const path = require('path');
-const fs = require('fs');
 
 const dev = process.env.NODE_ENV !== 'production';
-const hostname = 'localhost';
-const port = process.env.PORT || 4000;
+const hostname = process.env.HOSTNAME || 'localhost';
+const port = process.env.PORT || 3000;
 
 const app = next({ dev, hostname, port });
 const handle = app.getRequestHandler();
@@ -15,23 +13,18 @@ app.prepare().then(() => {
   createServer(async (req, res) => {
     try {
       const parsedUrl = parse(req.url, true);
-      const { pathname } = parsedUrl;
-
-      // Handle static files
-      if (pathname.startsWith('/_next/')) {
-        app.serveStatic(req, res, path.join(__dirname, '.next', pathname));
-        return;
-      }
-
-      // Handle other requests
       await handle(req, res, parsedUrl);
     } catch (err) {
       console.error('Error occurred handling', req.url, err);
       res.statusCode = 500;
       res.end('Internal Server Error');
     }
-  }).listen(port, (err) => {
-    if (err) throw err;
-    console.log(`> Ready on http://${hostname}:${port}`);
-  });
+  })
+    .once('error', (err) => {
+      console.error(err);
+      process.exit(1);
+    })
+    .listen(port, () => {
+      console.log(`> Ready on http://${hostname}:${port}`);
+    });
 }); 
