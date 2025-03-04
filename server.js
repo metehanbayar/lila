2da93c6317b0410fa4845922ac262df000581f97
Metehan Bayar <metehan.bayar@gmail.com>
2025-03-04 16:04:47 +0300

    port

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const fs = require('fs');
 
 const dev = process.env.NODE_ENV !== 'production';
 const hostname = 'localhost';
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 
 const app = next({ dev, hostname, port });
 const handle = app.getRequestHandler();
