5a47f4c20ca9937d08db761938685a7d39d16b95
Metehan Bayar <metehan.bayar@gmail.com>
2025-03-05 15:37:06 +0300

    pm2

--- next.config.js
+++ next.config.js
@@ -2,8 +2,6 @@
 const nextConfig = {
   reactStrictMode: true,
   output: 'standalone',
-  distDir: '.next',
-  trailingSlash: false,
   images: {
     domains: ['placehold.co'],
     unoptimized: true
@@ -13,25 +11,6 @@ const nextConfig = {
     MSSQL_PASSWORD: process.env.MSSQL_PASSWORD,
     MSSQL_SERVER: process.env.MSSQL_SERVER,
     MSSQL_DATABASE: process.env.MSSQL_DATABASE,
-  },
-  experimental: {
-    serverActions: {
-      allowedOrigins: ['localhost:3000', 'qr.lila.company']
-    }
-  },
-  // PWA config
-  headers: async () => {
-    return [
-      {
-        source: '/service-worker.js',
-        headers: [
-          {
-            key: 'Cache-Control',
-            value: 'public, max-age=0, must-revalidate',
-          },
-        ],
-      },
-    ]
   }
 }
 
